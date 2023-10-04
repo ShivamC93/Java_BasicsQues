0daf49f4fabf328e0944e1a194220041158b6d5b
Shivam Chaugule <114950843+ShivamC93@users.noreply.github.com>
2023-10-04 19:04:51 +0530

    Add files via upload

---
+++ areaofCircleInput.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class areaofCircleInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of radius: ");
+        int radius = sc.nextInt();
+        double pi = 3.14;
+        double area = pi*radius*radius;
+        System.out.println(area);
+
+
+
+    }
+    
+
+    
+}
